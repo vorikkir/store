10c74555062f9244d3eaf6835253b462b06484b9
Your Name <sisoko1110ok>
2020-02-06 14:10:40 +0300

    2коммит 05/02/2020

--- store/urls.py
+++ store/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from account import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
+    path('', views.index, name='index'),
 ]
