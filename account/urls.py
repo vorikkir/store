dceec22e5e7d68bfd0ac4ab9cfb445db6b955f4f
Your Name <sisoko1110ok>
2020-02-26 16:08:44 +0300

    Изменение пароля пользователя

--- account/urls.py
+++ account/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
--- account/views.py
+++ account/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm, UserRegistrationForm
-from django.contrib.auth.decorators import login_required
+
 
 
 def user_login(request):
